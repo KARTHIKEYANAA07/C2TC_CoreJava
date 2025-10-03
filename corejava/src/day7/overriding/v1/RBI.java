0883c76ecb47d0df2dfd2e7e138974be7ccaebdb
karthikeyan A <jaakkarthi10@gmail.com>
2025-10-03 18:49:10 +0530

    commit by karthik

---
+++ corejava/src/day7/overriding/v1/RBI.java
@@ -0,0 +1,12 @@
+
+package day7.overriding;
+
+public class RBI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public float getRateOfInterest() {
+			return 6.7f;
+	}
+
+}
---
+++ corejava/src/day7/overriding/v1/SBI.java
@@ -0,0 +1,18 @@
+
+
+
+public class SBI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class SBI extends RBI {
+			@Override
+			public float getRateOfInterest() {
+				return 7.0f;
+			}
+
+			public SBI getObject() {
+				return this;
+	}
+
+}
